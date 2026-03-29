4261be56a14baebeae3abde4e68125941ed95aca
Samuel Kroiß <mail@samuelkroiss.de>
2026-03-29 16:17:04 +0200

    Create ServiceProvider.php

---
+++ src/ServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Semmelsamu\Imgs;
+
+use Illuminate\Support\ServiceProvider;
+
+class ServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+    }
+
+    public function register(): void
+    {
+    }
+}
