cdef2fc312ffc43311ad44784d3baa9054b54dcb
Samuel Kroiß <mail@samuelkroiss.de>
2026-03-29 16:27:36 +0200

    Fix namespace and declare strict types

--- src/Image.php
+++ src/Image.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\View\Components;
+namespace Semmelsamu\Imgs;
 
 use Closure;
 use Exception;
--- src/Optimize.php
+++ src/Optimize.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace App\Console\Commands;
+declare(strict_types=1);
+
+namespace Semmelsamu\Imgs;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
